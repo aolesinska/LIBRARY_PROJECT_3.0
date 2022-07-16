294ec39d9158610699d183c857f7eba28004fb0a
aolesinska <aleksandra.olesinska@microsoft.wsei.edu.pl>
2022-07-16 12:02:31 +0200

    Create PublisherWindow

--- LIBRARY_PROJECT_3.0/Dals/PublisherDal.cs
+++ LIBRARY_PROJECT_3.0/Dals/PublisherDal.cs
@@ -27,5 +27,57 @@ namespace LIBRARY_PROJECT_3._0.Dals
                     ID = publisher.ID,
                     Name = publisher.Name,                    
                 }).ToList();
+
+        internal void Add(string name, string city, string street, string num, string post)
+        {
+            var newPublisher = new Publisher()
+            {
+                Name = name,
+                City = city,
+                Street = street,
+                BuildingNum = num,
+                Postcode = post
+            };
+
+            db.Publishers.Add(newPublisher);
+            db.SaveChanges();
+        }
+
+        internal void Update(string name, string nameUpdate, string city, string street, string num, string post)
+        {
+            var publisherToUpdate = db.Publishers
+                .Where(pub => pub.Name == name)
+                .Select(pub => pub)
+                .Single();
+
+            if (publisherToUpdate != null)
+            {
+                publisherToUpdate.Name = nameUpdate;
+                publisherToUpdate.City = city;
+                publisherToUpdate.Street = street;
+                publisherToUpdate.BuildingNum = num;
+                publisherToUpdate.Postcode = post;
+            }
+            db.SaveChanges();
+        }
+
+        internal void Delete(string name)
+        {
+            var publisherToDelete =
+                db.Publishers
+                .Where(pub => pub.Name == name)
+                .Select(pub => pub)
+                .Single();
+            db.Books
+                .Where(book => book.PublisherID == publisherToDelete.ID)
+                .ToList()
+                .ForEach(x =>
+                {
+                    x.PublisherID = null;
+                });
+
+            db.Publishers.Remove(publisherToDelete);
+            db.SaveChanges();
+        }
     }
 }
