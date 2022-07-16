436b1c8157ca759d62d3e9ceafa0b20344f5d277
aolesinska <aleksandra.olesinska@microsoft.wsei.edu.pl>
2022-07-16 12:53:43 +0200

    Add ReaderWindow

--- LIBRARY_PROJECT_3.0/Dals/ReaderDal.cs
+++ LIBRARY_PROJECT_3.0/Dals/ReaderDal.cs
@@ -20,5 +20,51 @@ namespace LIBRARY_PROJECT_3._0.Dals
                     Email = reader.Email,
                     Phone = reader.Phone,
                 }).ToList();
+
+        internal void Add(string firstname, string lastname, string pesel, string email, string phone)
+        {
+            var newReader = new Reader()
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                PESEL = pesel,
+                Email = email,
+                Phone = phone
+            };
+
+            db.Readers.Add(newReader);
+            db.SaveChanges();
+        }
+
+        internal void Update(string email, string firstname, string lastname, string pesel, string emailUpdate, string phone)
+        {
+            var readerToUpdate = db.Readers
+                .Where(read => read.Email == email)
+                .Select(read => read)
+                .Single();
+
+            if (readerToUpdate != null)
+            {
+                readerToUpdate.FirstName = firstname;
+                readerToUpdate.LastName = lastname;
+                readerToUpdate.PESEL = pesel;
+                readerToUpdate.Email = emailUpdate;
+                readerToUpdate.Phone = phone;
+            }
+            db.SaveChanges();
+        }
+
+        internal void Delete(string email)
+        {
+            var readerToDelete =
+                db.Readers
+                .Where(pub => pub.Email == email)
+                .Select(pub => pub)
+                .Single();
+            
+            db.Readers.Remove(readerToDelete);
+            db.SaveChanges();
+        }
     }
+
 }
