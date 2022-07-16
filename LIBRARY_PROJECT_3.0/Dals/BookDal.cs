2dae9e3262489ddc50c4c78af92b59445b7a7ccd
aolesinska <aleksandra.olesinska@microsoft.wsei.edu.pl>
2022-07-16 10:26:42 +0200

    Create BookWindow

--- LIBRARY_PROJECT_3.0/Dals/BookDal.cs
+++ LIBRARY_PROJECT_3.0/Dals/BookDal.cs
@@ -23,5 +23,30 @@ namespace LIBRARY_PROJECT_3._0.Dals
                     Publisher = book.Publisher.Name,
                     Status = book.Status.Status1,                    
                 }).ToList();
-    }
+        public void Add(string title, string isbn, int categoryID, int authorId, int publisherID, int statusId)
+        {
+            var newBook = new Book()
+            {
+                Title = title,
+                ISBN = isbn,
+                CategorieID = categoryID,
+                AutorID = authorId,
+                PublisherID = publisherID,
+                StatusID = statusId,
+            };
+
+            db.Books.Add(newBook);
+            db.SaveChanges();
+        }
+        public void Delete(string title)
+        {
+            var bookToDelete = db.Books
+                .Where(book => book.Title == title)
+                .Select(book => book)
+                .FirstOrDefault();
+
+            db.Books.Remove(bookToDelete);
+            db.SaveChanges();
+        }
+    }    
 }
